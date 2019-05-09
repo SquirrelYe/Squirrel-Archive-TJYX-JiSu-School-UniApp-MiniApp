0594e4bbf6e695654ece2e93e7366866f536fa55
yexuan <yexuan@yexuan.site>
2019-05-09 16:24:23 +0800

    init project …… use a template ....

---
+++ pages/flow/flow.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="app">
+		<waterfall-flow :list="list" :loading="loading" @click="choose"></waterfall-flow>
+	</view>
+</template>
+
+<script>
+	// 瀑布流组件
+    import WaterfallFlow from '@/components/nairenk-waterfall-flow/nairenk-waterfall-flow.vue';
+    // 模拟 JSON 数据
+    var data = require('@/common/json/data.json');
+    export default {
+        data() {
+            return {
+                page: 1,
+                start: 0,
+                end: 0,
+				loading: true, // 加载动画
+                list: [] // 列表
+            }
+        },
+        mounted() {
+            this.getList();
+        },
+        // 触底加载更多
+        onReachBottom() {
+            this.page++;
+			this.loading = true;
+            this.getList();
+        },
+        methods: {
+			// 选中
+			choose(item) {
+				// item 返回选中 JSON 对象
+				console.log(item)
+			},
+            // 模拟数据加载
+            getList() {
+                if (this.list.length < data.list.length) {
+                    setTimeout(() => {
+                        this.end = this.page * 10;
+                        this.list = this.list.concat(data.list.slice(this.start, this.end));
+                        this.start = this.end;
+						// 延迟 120 毫秒隐藏加载动画，为了跟组件里面的 100 毫秒定位有个平缓过度
+						setTimeout(() => {
+							this.loading = false;
+						}, 120);
+					}, 1000)
+				} else {
+					this.loading = false;
+				}
+            }
+        },
+        components: {
+            WaterfallFlow
+        }
+    }
+</script>
+
+<style>
+	.app {
+		height: 100%;
+	}
+</style>
