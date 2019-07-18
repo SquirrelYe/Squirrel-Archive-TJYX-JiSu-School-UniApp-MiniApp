7fc8af783424619243251ac382d6e16e9df17846
willye(叶武) <willye@tencent.com>
2019-07-18 15:28:47 +0800

    modified force logout logic

--- utils/req/request.js
+++ utils/req/request.js
@@ -1,3 +1,4 @@
+import store from '../../store/index'
 export default class Request {
 	constructor(arg) { this.request(arg) }
 	config = {
@@ -72,6 +73,7 @@ export default class Request {
 			content:'用户在另一处登录',
 			complete() {
 				// 清除缓存
+				store.commit('logout')
 				uni.clearStorage()
 				// 重定向
 				uni.redirectTo({ url:"/pages/public/login" })
